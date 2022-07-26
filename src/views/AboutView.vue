13ee0e0f118afa1f8c1248638a12e0a035ad81a5
zeroEuphemia <1084327995@qq.com>
2022-07-26 17:53:16 +0800

    Changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <transition appear @before-enter="beforeEnter"
+                @enter="enter" @after-enter="afterEnter">
+      <h1>This is an about page</h1>
+    </transition>
   </div>
 </template>
 <style scoped>
@@ -8,3 +11,29 @@
   color: red;
 }
 </style>
+
+<script>
+import gsap from 'gsap'
+
+export default {
+  setup() {
+    const beforeEnter = (el) => {
+      console.log('before-enter set initial state', el);
+      el.style.transform = 'translateY(-60px)';
+      el.style.opacity = 0;
+    }
+    const enter = (el, done) => {
+      console.log('enter', el);
+      gsap.to(el, {y: 0,
+        opacity: 1,
+        duration: 3,
+        ease: 'bounce.out',
+        onComplete: done});
+    }
+    const afterEnter = (el) => {
+      console.log('after-enter', el);
+    }
+    return {beforeEnter, enter, afterEnter}
+  }
+}
+</script>
