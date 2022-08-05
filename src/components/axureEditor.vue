cd0331635775f504be2f2e99203f8a68ff16418d
luoyashuo <914358195@qq.com>
2022-08-05 13:22:02 +0800

    projectdetail路由没加

--- src/components/axureEditor.vue
+++ src/components/axureEditor.vue
@@ -1,6 +1,16 @@
 <template>
-  <img alt="共享文档" src="@/assets/img/fileicons/chengxu.png" v-show="!this.isNew" class="fileicon">
-  <img alt="共享文档" src="@/assets/img/fileicons/newfile.png" v-show="this.isNew" class="fileicon">
+  <img alt="共享文档" src="@/assets/img/fileicons/chengxu.png"
+       v-show="!this.isNew" class="fileicon"
+       @click="this.$router.push({
+       name: 'axure',})"
+
+  >
+  <img alt="共享文档" src="@/assets/img/fileicons/newfile.png"
+       v-show="this.isNew" class="fileicon"
+       @click="this.$router.push({
+       name: 'axure',
+       })"
+  >
 </template>
 
 <script>
--- src/utils/api.js
+++ src/utils/api.js
@@ -18,7 +18,7 @@ export async function register(data) {
 
 export function save_uml(data){
     return request({
-        url: 'api/team/uml/save',
+        url: 'api/save/xml',
         method: 'post',
         data,
     })
@@ -26,14 +26,14 @@ export function save_uml(data){
 
 export function create_uml(data){
     return request({
-        url: 'api/team/doc/load',
+        url: 'api/new/xml',
         method: 'post',
         data,
     })
 }
 export function load_uml(data){
     return request({
-        url: 'api/team/uml/load',
+        url: 'api/load/xml',
         method: 'post',
         data,
     })
@@ -41,7 +41,7 @@ export function load_uml(data){
 
 export function del_uml(data){
     return request({
-        url: 'api/team/uml/load',
+        url: 'api/delete/xml',
         method: 'post',
         data,
     })
@@ -80,7 +80,7 @@ export async function changepassword(data) {
 
 export function save_doc(data){
     return request({
-        url: 'api/team/doc/save',
+        url: 'api/save/doc',
         method: 'post',
         data,
     })
@@ -88,7 +88,7 @@ export function save_doc(data){
 
 export function load_doc(data){
     return request({
-        url: 'api/team/doc/load',
+        url: 'api/load/doc',
         method: 'post',
         data,
     })
@@ -96,7 +96,7 @@ export function load_doc(data){
 
 export function del_doc(data){
     return request({
-        url: 'api/team/doc/load',
+        url: 'api/delete/doc',
         method: 'post',
         data,
     })
@@ -104,7 +104,7 @@ export function del_doc(data){
 
 export function create_doc(data){
     return request({
-        url: 'api/team/doc/load',
+        url: 'api/new/doc',
         method: 'post',
         data,
     })
