048e47da34350cf76f732b4c8ab3fa911cd16add
xiao7 <3499475017@qq.com>
2022-12-15 11:51:55 +0800

    Update alioss.js

--- src/assets/alioss.js
+++ src/assets/alioss.js
@@ -2,10 +2,7 @@
 import OSS from "ali-oss";
 export function client(){
   var client = new OSS({
-      endpoint: 'oss-cn-hangzhou.aliyuncs.com',//填写Bucket所在地域
-      accessKeyId: 'LTAI5tNTp64pvj7ZyM9GBBrB',
-      accessKeySecret: 'uraarRMA75smURHejuKbVw6IhMgxWM',
-      bucket: 'miaotu-headers',// 填写Bucket名称。
+
   })  //后端提供数据 
   return client
 }
